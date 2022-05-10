6011e142aa3d058fa2d775066628268a9e81baa5
erincochanco <91221052+erincochanco@users.noreply.github.com>
2022-05-10 18:24:51 -0400

    mobile-version

---
+++ safe-space-mobile/script.js
@@ -0,0 +1,20 @@
+const units = document.querySelectorAll('.grid-container > *'); // every child item in directly in all the grid containers
+const characters = ['.','|','&bsol;', '(', ')', '•', '-', '_',':','/','@','>','<']; // array of possible character list in order to cycle through
+
+// for every HTML unit element...
+units.forEach(unit => {
+  let charNum = 0; // set an initial character counter of 0
+  // add a click event to the html unit...
+  unit.addEventListener('click', () => {
+    unit.innerHTML = characters[charNum]; // on click, replace the html with the characters index that matches the current charNum value. 0, 1, 2, etc.
+    charNum = charNum >= characters.length - 1 ? 0 : charNum + 1; // update the counter to increase by 1 unless it's at the end of the characters array, then go back to 0
+  });
+});
+
+function myFunction() {
+  let person = prompt("Please enter a name", "Anything but Karen");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hey-o, my name is " + person + "!";
+  }
+}
